750240ce42d3c2e76b5852d37ee528b5b89d8d2c
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:14:37 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,16 @@ output "redis_cache_access_policies" {
   description = "All redis_cache_access_policy resources"
   value       = azurerm_redis_cache_access_policy.redis_cache_access_policies
 }
+output "redis_cache_access_policies_name" {
+  description = "List of name values across all redis_cache_access_policies"
+  value       = [for k, v in azurerm_redis_cache_access_policy.redis_cache_access_policies : v.name]
+}
+output "redis_cache_access_policies_permissions" {
+  description = "List of permissions values across all redis_cache_access_policies"
+  value       = [for k, v in azurerm_redis_cache_access_policy.redis_cache_access_policies : v.permissions]
+}
+output "redis_cache_access_policies_redis_cache_id" {
+  description = "List of redis_cache_id values across all redis_cache_access_policies"
+  value       = [for k, v in azurerm_redis_cache_access_policy.redis_cache_access_policies : v.redis_cache_id]
+}
 
