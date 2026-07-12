75182e6a3be8e8db23f1884614dc707872114d73
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:54 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "redis_cache_access_policies" {
-  description = "All redis_cache_access_policy resources"
-  value       = azurerm_redis_cache_access_policy.redis_cache_access_policies
-}
 output "redis_cache_access_policies_name" {
-  description = "List of name values across all redis_cache_access_policies"
-  value       = [for k, v in azurerm_redis_cache_access_policy.redis_cache_access_policies : v.name]
+  description = "Map of name values across all redis_cache_access_policies, keyed the same as var.redis_cache_access_policies"
+  value       = { for k, v in azurerm_redis_cache_access_policy.redis_cache_access_policies : k => v.name }
 }
 output "redis_cache_access_policies_permissions" {
-  description = "List of permissions values across all redis_cache_access_policies"
-  value       = [for k, v in azurerm_redis_cache_access_policy.redis_cache_access_policies : v.permissions]
+  description = "Map of permissions values across all redis_cache_access_policies, keyed the same as var.redis_cache_access_policies"
+  value       = { for k, v in azurerm_redis_cache_access_policy.redis_cache_access_policies : k => v.permissions }
 }
 output "redis_cache_access_policies_redis_cache_id" {
-  description = "List of redis_cache_id values across all redis_cache_access_policies"
-  value       = [for k, v in azurerm_redis_cache_access_policy.redis_cache_access_policies : v.redis_cache_id]
+  description = "Map of redis_cache_id values across all redis_cache_access_policies, keyed the same as var.redis_cache_access_policies"
+  value       = { for k, v in azurerm_redis_cache_access_policy.redis_cache_access_policies : k => v.redis_cache_id }
 }
 
